6bb624f7f22bd9f142b4ad9b9ab33eb3654d9677
Alec Mirambeau <alecmirambeau@gmail.com>
2024-11-21 14:55:07 -0500

    created basic layout of card item

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,9 +1,24 @@
 <template>
 <v-card class="pa-5 ma-1" max-height="400" max-width="300">
+
+    <v-card-item :title="product.name">
+        <template v-slot:subtitle>
+            <template v-for="i in product.rating" :key="i">
+                <v-icon color="yellow" icon="mdi-star"></v-icon>
+            </template>
+
+        </template>
+    </v-card-item>
+
 </v-card>
 
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+import { Product } from '../types/product';
+const product = defineProps<Product>()
+
 // your answer
 </script>
