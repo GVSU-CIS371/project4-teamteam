683cef5d17b0b193ffd11e538642f9f6560dcc87
prosperouspi <115018951+prosperouspi@users.noreply.github.com>
2024-11-22 14:46:14 -0500

    Update StoreItem.vue

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,24 +1,26 @@
 <template>
-<v-card class="pa-5 ma-1" height="400" >
-    <v-card-item class="ma-1" :title="product.name"></v-card-item>
+    <v-hover v-slot="{ isHovering, props }">
+        <v-card :class="{ 'on-hover': isHovering }" :elevation="isHovering ? 16 : 2" v-bind="props"
+            class="pa-5 mt-4 ml-4 mr-4" height="400">
+            <v-card-item class="mx-auto" :title="product.name"></v-card-item>
             <v-row class="flex-column" justify="center" align="center">
                 <v-row>
                     <v-col cols="12">
-            <!-- need to round otherwise it breaks application since can't loop over a decimal number. -->
-                        <v-icon v-for="i in Math.round(product.rating)" :key="i" color="yellow" icon="mdi-star"></v-icon>
+                        <!-- need to round otherwise it breaks application since can't loop over a decimal number. -->
+                        <v-icon v-for="i in Math.round(product.rating)" :key="i" color="yellow"
+                            icon="mdi-star"></v-icon>
+                        <v-icon v-for="i in (5 - Math.round(product.rating))" :key="i" color="yellow"
+                            icon="mdi-star-outline"></v-icon>
                         <v-icon color="green" icon="mdi-currency-usd"></v-icon>{{ product.price }}
                         <v-icon color="blue" icon="mdi-pound"></v-icon> {{ product.stock }}
 
-                    </v-col> 
-                    <!--  BELOW IS THE LINE OF CODE FOR EMPTY STAR ICON IF WANT TO IMPLEMENT LATER.-->
-                    <!-- <v-icon color="yellow" icon="mdi-star-outline"></v-icon> -->
+                    </v-col>
                 </v-row>
                 <img :src="product.image" alt="image of the item being displayed" height="190" width="150">
-                <p class="ma-2 pa-2" color="black">{{ product.description }}</p> 
+                <p class="ma-2 pa-2" color="black">{{ product.description }}</p>
             </v-row>
-
-
-</v-card>
+        </v-card>
+    </v-hover>
 
 </template>
 
